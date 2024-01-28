d90a9d301f9121cf8a7694e044dd23ae57664177
Artem Glebov <artem.glebov2004@yandex.ru>
2024-01-28 14:00:11 +0300

    сделал заголовок к странице About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,12 +1,18 @@
 const About = () => {
   return (
-    <div className="px-[108px] py-20">
-        <h1 className="text-[100px] uppercase text-white">Обо мне</h1>
-        <div>
+    <div className="px-[108px] py-20 flex items-center justify-between">
+      <h1 className="text-[100px] uppercase text-white">Обо мне</h1>
+      <div className="max-w-[705px] w-full">
+        <h2 className="text-white text-3xl">
+          Я front-end разработчик из Москвы. Получаю образование по
+          специальности "Прикладная информатика".
+        </h2>
+        <p>
           
-        </div>
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
